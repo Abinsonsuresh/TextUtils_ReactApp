b06e8dd032b28606284f3bd8319a7cc5d2303d33
Abinson Suresh <88340846+Abinsonsuresh@users.noreply.github.com>
2023-05-27 22:08:30 +0530

    All errors solved

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -48,21 +48,25 @@ export default function TextForm(props) {
           // style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}}
         ></textarea>
       </div>
-      <button className="btn btn-primary" onClick={UpperCase}>
+      <button className="btn btn-primary mx-2 my-1" onClick={UpperCase}>
         Convert to Uppercase
       </button>
-      <button className="btn btn-primary mx-2" onClick={LowerCase}>
+      <button className="btn btn-primary mx-2 my-1" onClick={LowerCase}>
         Convert to Lowercase
       </button>
-      <button className="btn btn-danger mx-2" onClick={clear}>
+      <button className="btn btn-danger mx-2 my-1" onClick={clear}>
         Clear
       </button>
       <div className="my-3" style={{color: props.mode==='light'?'black':'white'}}   >
         <h3 className="my-4">Text Summary</h3>
         <p>
-          {text.split(" ").length - 1}words and {text.length}characters
+          {text.split(" ").filter((element)=>{
+            return element.length!==0
+          }).length}words and {text.length}characters
         </p>
-        <p>{0.008 * text.split(" ").length - 1}Mins to read</p>
+        <p>{0.008 *text.split(" ").filter((element)=>{
+            return element.length!==0
+          }).length}Mins to read</p>
         <h2>Preview</h2>
         <p>{text.length>0? text: "Enter something to preview"}</p>
       </div>
